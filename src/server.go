0a64670f87ed1d8c2dd13a37954908a93ea73575
Koutarou Mori <m.koutarou2004@gmail.com>
2025-06-24 01:39:50 +0900

    monitoring events

--- src/server.go
+++ src/server.go
@@ -4,9 +4,11 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"io"
 	"math/big"
 	"os"
 	"os/signal"
+	"slices"
 	"strings"
 	"syscall"
 
@@ -20,8 +22,9 @@ import (
 	"github.com/rqlite/sql"
 )
 
+var airportCode [16]byte
+
 type Config struct {
-	AirportCode           string `json:"airportCode"`
 	RPCEndpoint           string `json:"rpcEndpoint"`
 	DispatcherAddr        string `json:"dispatcherAddr"`
 	DispatcherABIFilePath string `json:"dispatcherABIFilePath"`
@@ -65,10 +68,11 @@ func sendTransaction(contractAddr common.Address, txData []byte, amount *big.Int
 	if err != nil {
 		return err
 	}
-	senderAddress := crypto.PubkeyToAddress(privateKey.PublicKey)
+	senderAddr := crypto.PubkeyToAddress(privateKey.PublicKey)
 
 	msg := ethereum.CallMsg{
 		To:   &contractAddr,
+		From: senderAddr,
 		Data: txData,
 	}
 
@@ -78,12 +82,13 @@ func sendTransaction(contractAddr common.Address, txData []byte, amount *big.Int
 	}
 	defer client.Close()
 
-	gasLimit, err := client.EstimateGas(context.Background(), msg)
+	estimateGas, err := client.EstimateGas(context.Background(), msg)
 	if err != nil {
 		return err
 	}
+	gasLimit := uint64(float64(estimateGas) * 1.3)
 
-	nonce, err := client.PendingNonceAt(context.Background(), senderAddress)
+	nonce, err := client.PendingNonceAt(context.Background(), senderAddr)
 	if err != nil {
 		return err
 	}
@@ -152,25 +157,32 @@ func dispatcher() (common.Address, abi.ABI, error) {
 }
 
 func constructAirport() error {
-	config, err := loadConfig()
-	if err != nil {
-		return err
-	}
+	airportCode = uuid.New()
 
 	dispatcherAddr, dispatcherABI, err := dispatcher()
 	if err != nil {
 		return err
 	}
 
-	config.AirportCode = uuid.New().String()
-	txData, err := dispatcherABI.Pack("constructAirport", config.AirportCode)
+	txData, err := dispatcherABI.Pack("constructAirport", airportCode)
 
 	err = sendTransaction(dispatcherAddr, txData, nil)
 	if err != nil {
 		return err
 	}
 
-	err = saveConfig(config)
+	return err
+}
+
+func closeAirport() error {
+	dispatcherAddr, dispatcherABI, err := dispatcher()
+	if err != nil {
+		return err
+	}
+
+	txData, err := dispatcherABI.Pack("closeAirport", airportCode)
+
+	err = sendTransaction(dispatcherAddr, txData, nil)
 	if err != nil {
 		return err
 	}
@@ -179,25 +191,14 @@ func constructAirport() error {
 }
 
 func destroyAirport() error {
-	config, err := loadConfig()
-	if err != nil {
-		return err
-	}
-
 	dispatcherAddr, dispatcherABI, err := dispatcher()
 	if err != nil {
 		return err
 	}
 
-	txData, err := dispatcherABI.Pack("destroyAirport", config.AirportCode)
+	txData, err := dispatcherABI.Pack("destroyAirport", airportCode)
 
 	err = sendTransaction(dispatcherAddr, txData, nil)
-	if err != nil {
-		return err
-	}
-
-	config.AirportCode = ""
-	err = saveConfig(config)
 	if err != nil {
 		return err
 	}
@@ -205,112 +206,266 @@ func destroyAirport() error {
 	return nil
 }
 
-func checkNewRouteEvent() error {
-	config, err := loadConfig()
-	if err != nil {
-		return err
+func parseQuery(query string) (bool, string, error) {
+	write := false
+	var newQuery []string
+	p := sql.NewParser(strings.NewReader(query))
+	for {
+		stmt, err := p.ParseStatement()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return write, strings.Join(newQuery, "; ") + ";", err
+		}
+
+		switch stmt.(type) {
+		case *sql.InsertStatement, *sql.UpdateStatement, *sql.DeleteStatement:
+			newQuery = append(newQuery, stmt.String()+" RETURNING *")
+			write = true
+		case *sql.SelectStatement:
+			newQuery = append(newQuery, stmt.String())
+		default:
+			newQuery = append(newQuery, stmt.String())
+			write = true
+		}
 	}
 
+	return write, strings.Join(newQuery, "; ") + ";", nil
+}
+
+type Route struct {
+	Client            *ethclient.Client
+	Instance          chan struct{}
+	CanWriteAccount   []common.Address
+	DefaultPermission string
+}
+
+var routes = make(map[[16]byte]Route)
+
+func checkNewFlightPlanEvent(destination [16]byte) error {
 	dispatcherAddr, dispatcherABI, err := dispatcher()
 	if err != nil {
-		return err
+		fmt.Printf(">> Error: %s\n", err)
 	}
 
-	originTopic := []common.Hash{common.HexToHash("0x" + fmt.Sprintf("%x", []byte(config.AirportCode)))}
+	var destinationHash common.Hash
+	copy(destinationHash[:], destination[:])
 
 	query := ethereum.FilterQuery{
 		Addresses: []common.Address{dispatcherAddr},
 		Topics: [][]common.Hash{
-			{dispatcherABI.Events["NewRoute"].ID},
-			originTopic,
+			{dispatcherABI.Events["FlightPlanSubmitted"].ID},
+			{destinationHash},
 		},
 	}
 
-	client, err := ethclient.Dial(config.RPCEndpoint)
-	if err != nil {
-		return err
-	}
-	defer client.Close()
-
 	logs := make(chan types.Log)
-	sub, err := client.SubscribeFilterLogs(context.Background(), query, logs)
+	sub, err := routes[destination].Client.SubscribeFilterLogs(context.Background(), query, logs)
 	if err != nil {
-		return err
+		fmt.Printf(">> Error: %s\n", err)
 	}
 
-	sigs := make(chan os.Signal, 1)
-	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
-	defer signal.Stop(sigs)
-
-	fmt.Println(("Checking events..."))
+	fmt.Println(">> New route has been launched.")
 	for {
 		select {
-		case <-sigs:
-			fmt.Println("Signal received, exiting loop...")
+		case <-routes[destination].Instance:
+			fmt.Println(">> The route has been terminated.")
 			return nil
 		case <-sub.Err():
 			continue
 		case vLog := <-logs:
 			var event struct {
-				Origin            string
-				Destination       string
+				Destination [16]byte
+				Query       string
+				Operator    common.Address
+			}
+
+			err := dispatcherABI.UnpackIntoInterface(&event, "FlightPlanSubmitted", vLog.Data)
+			if err == nil {
+				write, query, err := parseQuery(event.Query)
+				if err != nil {
+					fmt.Printf(">> Error: %s\n", err)
+				}
+
+				fmt.Printf(">> %v\n", routes[destination].CanWriteAccount)
+				fmt.Printf(">> %s\n", event.Operator)
+
+				if write && slices.Contains(routes[destination].CanWriteAccount, event.Operator) {
+					fmt.Printf(">> Write Query: %s\n", query)
+					// SQLを実行
+					// RETURNINGのハッシュを計算
+					// ハッシュの送信
+				} else if !write {
+					fmt.Printf(">> Read Query: %s\n", query)
+				} else {
+					fmt.Println(">> Permission error.")
+				}
+			}
+		}
+	}
+}
+
+func checkRouteTerminatedEvent(client *ethclient.Client, airportInstance chan struct{}) error {
+	dispatcherAddr, dispatcherABI, err := dispatcher()
+	if err != nil {
+		fmt.Printf(">> Error: %s\n", err)
+	}
+
+	var originHash common.Hash
+	copy(originHash[:], airportCode[:])
+
+	query := ethereum.FilterQuery{
+		Addresses: []common.Address{dispatcherAddr},
+		Topics: [][]common.Hash{
+			{dispatcherABI.Events["RouteTerminated"].ID},
+			{originHash},
+		},
+	}
+
+	logs := make(chan types.Log)
+	sub, err := client.SubscribeFilterLogs(context.Background(), query, logs)
+	if err != nil {
+		fmt.Printf(">> Error: %s\n", err)
+	}
+
+	fmt.Println(">> Your airport has been opened (1/2).")
+	for {
+		select {
+		case <-airportInstance:
+			fmt.Println(">> Your airport has been closed (1/2).")
+			return nil
+		case <-sub.Err():
+			continue
+		case vLog := <-logs:
+			var event struct {
+				Origin      [16]byte
+				Destination [16]byte
+			}
+
+			err := dispatcherABI.UnpackIntoInterface(&event, "RouteTerminated", vLog.Data)
+			if err == nil {
+				fmt.Println(">> The route has been terminated.")
+
+				close(routes[event.Destination].Instance)
+				routes[event.Destination].Client.Close()
+
+				go checkNewFlightPlanEvent(event.Destination)
+			}
+		}
+	}
+}
+
+func checkNewRouteEvent(client *ethclient.Client, airportInstance chan struct{}) error {
+	dispatcherAddr, dispatcherABI, err := dispatcher()
+	if err != nil {
+		fmt.Printf(">> Error: %s\n", err)
+	}
+
+	var originHash common.Hash
+	copy(originHash[:], airportCode[:])
+
+	query := ethereum.FilterQuery{
+		Addresses: []common.Address{dispatcherAddr},
+		Topics: [][]common.Hash{
+			{dispatcherABI.Events["NewRouteLaunched"].ID},
+			{originHash},
+		},
+	}
+
+	logs := make(chan types.Log)
+	sub, err := client.SubscribeFilterLogs(context.Background(), query, logs)
+	if err != nil {
+		fmt.Printf(">> Error: %s\n", err)
+	}
+
+	fmt.Println(">> Your airport has been opened (2/2).")
+	for {
+		select {
+		case <-airportInstance:
+			fmt.Println(">> Your airport has been closed (2/2).")
+			return nil
+		case <-sub.Err():
+			continue
+		case vLog := <-logs:
+			var event struct {
+				Origin            [16]byte
+				Destination       [16]byte
 				CanWriteAccount   []common.Address
 				DefaultPermission string
 			}
 
-			err := dispatcherABI.UnpackIntoInterface(&event, "NewRoute", vLog.Data)
+			err := dispatcherABI.UnpackIntoInterface(&event, "NewRouteLaunched", vLog.Data)
 			if err == nil {
-				continue
+				fmt.Println(">> New route has been launched.")
+				config, err := loadConfig()
+				if err != nil {
+					fmt.Printf(">> Error: %s\n", err)
+				}
+
+				client, err := ethclient.Dial(config.RPCEndpoint)
+				if err != nil {
+					fmt.Printf(">> Error: %s\n", err)
+				}
+
+				routes[event.Destination] = Route{
+					client,
+					make(chan struct{}),
+					event.CanWriteAccount,
+					event.DefaultPermission,
+				}
+
+				go checkNewFlightPlanEvent(event.Destination)
 			}
 		}
 	}
 }
 
-func parseQuery(query []string) ([]string, error) {
-	newQuery := []string{}
-	for _, q := range query {
-		parser := sql.NewParser(strings.NewReader(q))
-		stmt, err := parser.ParseStatement()
+func main() {
+	if airportCode == [16]byte{} {
+		err := constructAirport()
 		if err != nil {
-			return newQuery, err
-		}
-
-		switch stmt.(type) {
-		case *sql.InsertStatement, *sql.UpdateStatement, *sql.DeleteStatement:
-			newQuery = append(newQuery, strings.TrimRight(strings.TrimSpace(q), ";")+" RETURNING *;")
+			panic(err)
 		}
 	}
 
-	return newQuery, nil
-}
+	fmt.Println(">> Hello! President!")
+	fmt.Println(">> Here is airport console (Press Ctrl+C to exit).")
+	fmt.Printf(">> Your airport code is %x.\n", airportCode)
 
-func main() {
+	airportInstance := make(chan struct{})
+
 	config, err := loadConfig()
 	if err != nil {
 		panic(err)
 	}
 
-	if config.AirportCode == "" {
-		err = constructAirport()
-		if err != nil {
-			panic(err)
-		}
-
-		config, err = loadConfig()
-		if err != nil {
-			panic(err)
-		}
-	}
-
-	err = checkNewRouteEvent()
+	client, err := ethclient.Dial(config.RPCEndpoint)
 	if err != nil {
 		panic(err)
 	}
+
+	go checkNewRouteEvent(client, airportInstance)
+	go checkRouteTerminatedEvent(client, airportInstance)
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+	<-sigChan
+
+	close(airportInstance)
+	client.Close()
+
+	err = closeAirport()
+	if err != nil {
+		panic(err)
+	}
+
+	// マイグレートを実行
 
 	err = destroyAirport()
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println("Destroyed")
+	fmt.Println(">> Bye!")
 }
